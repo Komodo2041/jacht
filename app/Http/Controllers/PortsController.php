bd0b05c16243d1ff68fcd454abecefa455cbef8a
Łukasz Jaroszuk <krortop@gmail.com>
2025-11-26 12:36:26 +0100

    Add First Table Ports

---
+++ app/Http/Controllers/PortsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+namespace App\Models\Ports;
+
+class PortsController extends Controller
+{
+    //
+}
